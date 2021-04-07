b7a3d17180277ff58b4cb67e569314c29dcb55a0
mbm_1607 <muhammadkhan1607@gmail.com>
2021-04-07 15:08:17 +0500

    Add line numbers to editor

--- src/Editor.js
+++ src/Editor.js
@@ -8,11 +8,21 @@ const Editor = ({ text, setText }) => {
 	}
 
 	return (
-		<textarea
-			id="editor"
-			onChange={onChange}
-			value={text} >
-		</textarea>
+		<div id='editor-container'>
+			<p id='line-numbers'>
+				{
+					text.split('\n').map((_, i) => {
+						return `${i}\n`;
+					})
+				}
+			</p>
+			<textarea
+				id='editor'
+				onChange={onChange}
+				value={text}
+			>
+			</textarea>
+		</div>
 	)
 };
 
